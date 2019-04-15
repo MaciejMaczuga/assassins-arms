7a10477591e35e804fbc3d9a609f4baa95f49375
Maczuga Maciej <maczugamaciek@gmail.com>
2019-04-16 01:07:26 +0200

    +navigation structure for drop down navigation menu including lvl2 and3 +styles for naigation menu and hover effects +jquery script to toggle class for showing/hiding dropdown nav section +minor css updates

--- js/script.js
+++ js/script.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
     $('.flag').on('click', function() {
         $('.lang-menu').toggleClass('hidden');
     })
+
+    $('.navigation-lvl-1-cat > a').on('mouseenter', function(event) {
+        $(event.target).next('ul').removeClass('nav-hidden')
+    })
+    $('.navigation-lvl-1-cat > a').on('mouseleave', function(event) {
+        $(event.target).next('ul').addClass('nav-hidden')
+    })
 })
