ba4f6bc83d0b571d03231c76b91e5eae1e9169a2
Maczuga Maciej <maczugamaciek@gmail.com>
2019-04-16 18:56:27 +0200

    +further styling of product detail section +changed format of some icons to png

--- js/script.js
+++ js/script.js
@@ -1,12 +1,62 @@
 $(document).ready(function() {
+    // checkWindowWidth();
+
+    // toggle language menu
     $('.flag').on('click', function() {
         $('.lang-menu').toggleClass('hidden');
-    })
+    });
 
-    $('.navigation-lvl-1-cat > a').on('mouseenter', function(event) {
-        $(event.target).next('ul').removeClass('nav-hidden')
-    })
-    $('.navigation-lvl-1-cat > a').on('mouseleave', function(event) {
-        $(event.target).next('ul').addClass('nav-hidden')
-    })
-})
+    // $(window).on("resize", function(){
+    //     checkWindowWidth();
+    // });
+
+    // function rwdDomRebuild(mobile, desktop) {
+    //     if (mobile) {
+    //     let $nav = $('nav').detach()
+    //     let $contact = $('.header-contact').detach()
+    //     let $myAcc = $('.lang-settings').detach()
+    //     let $langMenu = $('.my-account').detach()
+    //     }
+    //     if (desktop) {
+    //         $('.wrapper--bg-white').append($nav)
+    //     }
+    // }
+
+    // function checkWindowWidth(){
+    //     $vWidth = $(window).width();
+    //     if($vWidth < 768){
+    //         rwdDomRebuild('mobile')
+    //     } else {
+    //         rwdDomRebuild('desktop')
+    //     }
+    // }
+
+
+    $('.slider-photos').slick({
+        infinite: true,
+        vertical: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        nextArrow: '<button type="button" class="arrow-down"><img src="./img/slick-down-arrow.png"></button>',
+        prevArrow: '<button type="button" class="arrow-up"><img src="./img/slick-up-arrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                }
+            },
+            {
+              breakpoint: 760,
+              settings: {
+                dots: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                vertical: false,
+                arrows: false
+              }
+            }
+          ]
+    });
+
+});
