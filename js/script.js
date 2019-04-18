5270792ddb7a3cb52c1c7680b922c438cfc1f3be
Maczuga Maciej <maczugamaciek@gmail.com>
2019-04-18 21:18:25 +0200

    -removed unused slick files +upgraded slick to newer version +changed starting parameters of slick +changed styles of slick sliders +did some small styling in almos all sections for mobile and small screens +started improving mobile menu. For now most of header and nav elemets are moved under hidden mobile menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
+// get all elements needed for mobile menu
+
+
+  function reorganize() {
+    var menu = $('#menu');
+    var search = $('.search-wrapper');
+    var lang = $('.lang-menu');
+    var contact = $('.header-contact');
+    var account = $('.my-account')
+    menu.prepend(search);
+    menu.append([account, contact, lang]);
+  }
+  reorganize();
+
 
     $('.flag').on('click', function() {
         $('.lang-menu').toggleClass('hidden');
     });
 
 
+    // $('.slick').on('beforeChange', function(event,slick,slide,nextSlide) {
+    //   $('.slider-nav-thumbnails').find('.slick-slide').removeClass('slick-current').eq(nextSlide).addClass('slick-current');
+    //   });
 
 
     $('.slider-photos').slick({
         infinite: true,
         vertical: true,
+        focusOnSelect: true,
         slidesToShow: 6,
         slidesToScroll: 1,
         nextArrow: '<button type="button" class="arrow-down"><img src="./img/slick-down-arrow.png"></button>',
@@ -18,7 +36,7 @@ $(document).ready(function() {
             {
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 5,
+                    slidesToShow: 6,
                 }
             },
             {
@@ -40,5 +58,7 @@ $(document).ready(function() {
           collapsible: false
         });
       } );
+
+
 
 });
